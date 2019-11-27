2cdbd44ddbb34f38ba1cd945b68379f16cc6a910
htactive-nhanhdt <nhanhdt@htactive.com>
2019-11-27 23:34:22 +0700

    1134

--- src/components/Portfolio/banner.js
+++ src/components/Portfolio/banner.js
@@ -1,9 +1,36 @@
-import React from "react"
+import React,{useState, useEffect} from "react"
+import { ParallaxProvider, ParallaxBanner } from "react-scroll-parallax"
 
 const Banner = () => {
+  const [scrollY, setScrollY] = useState(0)
+  const logit = () => {
+    setScrollY(window.pageYOffset)
+  }
+  useEffect(() => {
+    function watchScroll() {
+      window.addEventListener("scroll", logit)
+    }
+    watchScroll()
+    return () => {
+      window.removeEventListener("scroll", logit)
+    }
+  }, [])
   return (
-    <div className="banner-page">
-      <div className="shield"></div>
+    <ParallaxProvider>
+    <ParallaxBanner
+      className="section parallax dark-translucent-bg parallax-bg-"
+      layers={[
+        {
+          image: bannerData.banner_img,
+          amount: 0.7,
+        },
+      ]}
+      style={{
+        marginTop: `${scrollY > 182 ? "147px" : "0"}`,
+        height: "400px",
+      }}
+    >
+    <div className="banner-page"> 
       <div className="container banner-lead">
         <div className="space-top"></div>
         <h1>Our Portfolios</h1>
@@ -17,6 +44,8 @@ const Banner = () => {
         </p>
       </div>
     </div>
+    </ParallaxBanner>
+    </ParallaxProvider>
   )
 }
 export default Banner
--- src/pages/services.js
+++ src/pages/services.js
@@ -8,54 +8,11 @@ import SEO from "../components/seo"
 import Banner from "../components/Services/banner"
 import WebApp from "../components/Services/webApp"
 
-export const query = graphql`
-  {
-    allMarkdownRemark(
-      filter: {
-        frontmatter: { banner_services: { banner_img: { regex: "/img/" } } }
-      }
-    ) {
-      edges {
-        node {
-          frontmatter {
-            banner_services {
-              banner_img
-            }
-          }
-        }
-      }
-    }
-  }
-`
-const Services = ({ data, language }) => {
-  const [scrollY, setScrollY] = useState(0)
-  const logit = () => {
-    setScrollY(window.pageYOffset)
-  }
-  useEffect(() => {
-    function watchScroll() {
-      window.addEventListener("scroll", logit)
-    }
-    watchScroll()
-    return () => {
-      window.removeEventListener("scroll", logit)
-    }
-  }, [])
+const Services = ({ language }) => {
   return (
     <Layout>
       <SEO title="Services" />
-      <Parallax
-        bgImage={
-          data.allMarkdownRemark.edges[0].node.frontmatter.banner_services
-            .banner_img
-        }
-        style={{marginTop:`${scrollY>182?"147px":"0"}`}}
-        strength={500}
-      >
-        <div style={{ height: 400 }}>
-          <Banner language={language} />
-        </div>
-      </Parallax>
+      <Banner language={language} />
       <WebApp language={language} />
     </Layout>
   )
