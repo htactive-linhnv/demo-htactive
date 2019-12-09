60f1022b3207622d6edd18c81df901d489c6bbcf
htactive-nhanhdt <nhanhdt@htactive.com>
2019-12-09 14:15:12 +0700

    css

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `HT-active site`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Your Satisfaction Is Our Success`,
+    author: `ht-active-team`,
   },
   plugins: [
     'gatsby-plugin-antd',
